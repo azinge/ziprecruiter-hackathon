158d6d97793ccd82448694b60af4f970f86889dd
mvg210@gmail.com <mikegnanakone@Mikes-MacBook-Pro.local>
2017-05-06 14:41:44 -0700

    Added package.json

--- ziprecruiter/new-ziprecruiter/ziprecruiter/src/components/Registration.js
+++ ziprecruiter/new-ziprecruiter/ziprecruiter/src/components/Registration.js
@@ -4,7 +4,6 @@ export default class Registration extends React.Component {
   static propTypes = {
     name: React.PropTypes.string,
   };
-
   constructor(props) {
     super(props);
     this.state = {
@@ -12,7 +11,9 @@ export default class Registration extends React.Component {
       codeText: '',
       firstName: '',
       lastName: '',
-      fizzCode: ''
+      fizzCode: '',
+      userLocation: '',
+      bitcoinAddress: '',
     }
   }
 
@@ -61,9 +62,18 @@ while (n++ < 100) {
     lastName: e.target.value 
   });
   }
-
+  updateLocation(e) {
+    this.setState({
+      userLocation: e.target.value 
+    });
+  }
+  updateBitcoinAddress(e) {
+    this.setState({
+      bitcoinAddress: e.target.value 
+    });
+  }
   render() {
-    let { showCodeTest, codeText, fizzCode, firstName, lastName, loading } = this.state
+    let { showCodeTest, codeText, fizzCode, firstName, lastName, loading, userLocation, bitcoinAddress } = this.state
     
     return (
       <div className="container">
@@ -75,19 +85,47 @@ while (n++ < 100) {
         </a>
       </div>
         </div>
-        <div className={!showCodeTest ? 'hide' : ''}>
+        <div className={showCodeTest ? 'hide' : ''}>
           <div className="col s12">
-            <h4 className="headline">Create a FizzBuzz program</h4>
         <form onSubmit={this.sumbitForm.bind(this)}>
-          <input value={firstName} onChange={this.updateFirstName.bind(this)} placeholder="First Name" type="text" />
-          <input value={lastName} onChange={this.updateLastName.bind(this)} placeholder="Last Name" type="text" />
-          <textArea
-            placeholder="Write your code here"
-            onChange={this.updateCode.bind(this)}
-            className="code-form-input blue darken-4"
-            value={fizzCode}
-          >
-          </textArea>
+          <div className="row">
+              <div className="col s6">
+                <input value={firstName} onChange={this.updateFirstName.bind(this)} placeholder="First Name" type="text" />
+              </div>
+              <div className="col s6">
+                <input value={lastName} onChange={this.updateLastName.bind(this)} placeholder="Last Name" type="text" />
+              </div>
+              <div className="col s6">
+                <input value={userLocation} onChange={this.updateLocation.bind(this)} placeholder="What city are you in?" type="text" />
+              </div>
+              <div className="col s6">
+                <input value={bitcoinAddress} className="bitcoin-input" onChange={this.updateBitcoinAddress.bind(this)} placeholder="Bitcoin Address" type="text" />
+                <div className="switch charity-switch">
+                    <label>
+                      <input type="checkbox" />
+                      <span className="lever"></span>
+                      Donate to my prize charity
+                    </label>
+                  </div>
+              </div>
+              <div className="col s12">
+                <h4 className="headline">Create a FizzBuzz program</h4>
+                <ul>
+                  <li><span className="new badge">4</span></li>
+                  <li><span className="new badge">4</span></li>
+                  <li><span className="new badge">4</span></li>
+                  <li><span className="new badge">4</span></li>
+                  <li><span className="new badge">4</span></li>
+                </ul>
+                <textArea
+                  placeholder="Write your code here"
+                  onChange={this.updateCode.bind(this)}
+                  className="code-form-input blue darken-4"
+                  value={fizzCode}
+                >
+                </textArea>
+              </div>
+          </div>
           <input type="submit" className="btn right" value="Complete Test" />
         </form>
         { loading ? 'Loading...' : '' }
