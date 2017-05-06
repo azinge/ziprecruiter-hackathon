40c0d855217625a2fb634186678843ce395c6812
mvg210@gmail.com <mikegnanakone@Mikes-MacBook-Pro.local>
2017-05-06 12:15:04 -0700

    Added router

--- ziprecruiter/src/App.js
+++ ziprecruiter/src/App.js
@@ -1,14 +1,29 @@
-import React, {Component} from 'react';
-import './App.scss';
-import Registration from './components/Registration'
 
-class App extends Component {
-  render() {
+import React from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
+import Registration from './components/Registration'
+const Outer = (props) => <div><Links />{props.children}</div>;
+const Contact = () => <div><h1>Contact</h1></div>;
+
+
+const Links = () =>
+  <nav>
+    <Link to="/">Home</Link> 
+    <Link to="/register">Register</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+
+
+class App extends React.Component {
+  render(){
     return (
-      <div className="App">
-        <Registration />
-      </div>
-    )
+      <Router history={ hashHistory }>
+        <Route path="/" component={Outer}>
+        <Route path="register" component={Registration}></Route>
+        <Route path="contact" component={Contact}></Route>
+        </Route>
+      </Router>
+    );
   }
 }
 
