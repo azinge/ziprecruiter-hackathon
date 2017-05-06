53e7147a8dbb49d92d2969d9d8e90ee265087ae7
mvg210@gmail.com <mikegnanakone@Mikes-MacBook-Pro.local>
2017-05-06 12:00:12 -0700

    Working on registration

--- ziprecruiter/src/components/Registration.js
+++ ziprecruiter/src/components/Registration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles/Registration.scss';
 export default class Registration extends React.Component {
   static propTypes = {
     name: React.PropTypes.string,
@@ -7,16 +7,67 @@ export default class Registration extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+    	showCodeTest: false,
+    	codeText: '',
+    	fizzCode: ''
+    }
   }
 
+  showCodeTest() {
+  	let { showCodeTest } = this.state
+  	this.setState({
+  		showCodeTest: !showCodeTest,
+  		fizzCode: `let n = 0;
+while (n++ < 100) {
+  console.log(
+    n % 15 === 0 ? 'FizzBuzz' :
+    n % 5 === 0 ? 'Buzz' :
+    n % 3 === 0 ? 'Fizz' :
+    n
+  )
+}`,
+  	});
+  	
+  }
+  sumbitForm() {
+  	console.log("Submitted")
+  }
+  updateCode(e) {
+	this.setState({
+		codeText: e.target.value 
+	});
+  }
   render() {
+  	let { showCodeTest, codeText, fizzCode } = this.state
+  	
     return (
       <div className="container">
-      	<div className="row">
-			<div className="col s12">
+      	<div className={showCodeTest ? 'hide' : ''}>
+			<div className="col s12 center">
 				<h4>Turn your job search into a Hackathon</h4>
+				<a onClick={this.showCodeTest.bind(this)} className="btn">
+					Register Now
+				</a>
 			</div>
       	</div>
+      	<div className={!showCodeTest ? 'hide' : ''}>
+      		<div className="col s12">
+      			<h4>Create a FizzBuzz program</h4>
+				<form onSubmit={this.sumbitForm.bind(this)}>
+					<textArea
+						placeholder="Write your code here"
+						onChange={this.updateCode.bind(this)}
+						className="code-form-input blue darken-4"
+						value={fizzCode}
+					>
+						
+					</textArea>
+					<input type="submit" className="btn right" value="Complete Test" />
+				</form>
+      		</div>
+      	</div>
+
       </div>
     );
   }
